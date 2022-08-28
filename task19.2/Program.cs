6fa811dc3ae09acc216e4307c82e1c9c3c7d1cd0
Adalberto777 <albert.tliashev@gmail.com>
2022-08-29 00:24:07 +0500

    task19.2

---
+++ task19.2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("получаем на вход число пятизначное число и и проверяет, является ли оно палиндромом");
+
+Console.WriteLine("Enter a five-digit number");
+int number = int.Parse(Console.ReadLine());
+int digit = number;
+int newnumber = 0;
+int dig = 0;
+
+while (digit > 0)
+{
+    dig = digit % 10;
+    newnumber = newnumber * 10 + dig;
+    digit = digit / 10;
+}
+
+if(newnumber == number)
+{
+    Console.WriteLine("palindrome");
+} 
+else
+{
+    Console.WriteLine("not palindrome");
+}
