35e83d64fe237c8e8020430c5784dc4f8fcf1713
Adalberto777 <albert.tliashev@gmail.com>
2022-08-28 23:17:09 +0500

    task19.1

---
+++ task19.1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("получаем на вход число пятизначное число и и проверяет, является ли оно палиндромом");
+
+Console.WriteLine("Enter a five-digit number");
+
+string input = Console.ReadLine();
+int size = input.Length;
+
+if(size != 5)
+{
+    Console.WriteLine("It is't a five-digit number");
+}
+else
+{
+    if (input[0] == input[4] && input[1] == input[3])
+    {
+        Console.WriteLine("палиндром");
+    }
+    if (input[0] != input[4] | input[1] != input[3])
+    {
+        Console.WriteLine("не палиндром");
+    }
+}
+
+
