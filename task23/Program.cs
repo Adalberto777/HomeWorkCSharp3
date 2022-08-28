107b508fb5989dc6806386bf798ce623992cac5b
Adalberto777 <albert.tliashev@gmail.com>
2022-08-29 00:52:49 +0500

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("получаем на вход число N и выводит кубы чисел от 1 по N включительно");
+
+Console.WriteLine("Enter a integer number");
+
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i < N + 1)
+{
+    Console.Write($"{Math.Pow(i,3)},");
+    i++;
+}
